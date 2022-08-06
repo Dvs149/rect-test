7f7c0e21f07c13432e01af90054a319597d63869
Divyesh <149dvs@gmail.com>
2022-08-06 06:45:43 +0530

    changes 6

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          breaking changes 5
+          breaking changes 6
         </a>
       </header>
     </div>
